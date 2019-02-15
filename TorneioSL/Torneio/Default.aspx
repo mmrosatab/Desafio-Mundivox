b1cc1b5d8ef526ad0318178f315d8ef0f5a767d0
Mayara Marques <mmrosatab@hotmail.com>
2019-02-15 18:13:29 -0200

    Primeiro commit.

---
+++ TorneioSL/Torneio/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	<p>
+				<asp:Label ID="lNumChaves" runat="server" Text="N° de chaves:"></asp:Label>
+			</p>
+        </div>
+    	<p>
+			<asp:TextBox ID="tbNumChaves" runat="server"></asp:TextBox>
+		</p>
+		<p>
+			<asp:Label ID="lNumTimes" runat="server" Text="N° de times:"></asp:Label>
+		</p>
+		<p>
+			<asp:TextBox ID="tbNumTimes" runat="server"></asp:TextBox>
+		</p>
+		<asp:Button ID="btnCriarTorneio" runat="server" OnClick="btnCriarTorneio_Click" Text="Criar Torneio" />
+    </form>
+</body>
+</html>
---
+++ TorneioSL/Torneio/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnCriarTorneio_Click(object sender, EventArgs e)
+    {
+
+        int numChaves = Convert.ToInt32(tbNumChaves.Text);
+        int numTimes = Convert.ToInt32(tbNumTimes.Text);
+
+        if (numChaves >= 2 && numTimes >= 2 && numTimes >= numChaves)
+        {
+            if ((numChaves & (numChaves - 1)) == 0 && (numTimes & (numTimes - 1)) == 0)
+            {
+                Response.Write("Tudo Ok");
+
+            }
+            else
+            {
+                Response.Write("O n° de times e n° de chaves deve ser uma potencia de 2.");
+            }
+        }
+        else
+        {
+            Response.Write("O n° de times e n° de chaves deve ser maior ou igual a 2. ");
+            Response.Write("O n° de times deve ser maior que o n° de chaves.");
+        }
+    }
+}
+
